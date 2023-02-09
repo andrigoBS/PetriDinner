9fba008f9a90e3dd148b0a3d71cd389929f567ba
Andrigo Santos <andrigo.santos@luizalabs.com>
2023-02-09 00:44:55 -0300

    Class UML and main

--- src/petriDinner/network/Network.java
+++ src/petriDinner/network/Network.java
@@ -4,16 +4,40 @@ import petriDinner.network.transition.Transition;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Semaphore;
 
 public class Network {
     private List<Transition> transitions;
+    private int interactions;
+    private Semaphore semaphoreInteractions;
 
-    public Network(Transition transition){
+    public Network(){
         this.transitions = new ArrayList<>();
-        this.transitions.add(transition);
+        this.interactions = 0;
+        this.semaphoreInteractions = new Semaphore(1);
     }
 
     public void addTransition(Transition transition){
         this.transitions.add(transition);
+    }
+
+    private class TransactionThread extends Thread{
+        private Transition transition;
+
+        public TransactionThread(Transition transition) {
+            this.transition = transition;
+        }
+
+        @Override
+        public void run() {
+            transition.execute();
+            try {
+                semaphoreInteractions.acquire();
+                interactions++;
+                semaphoreInteractions.release();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
--- src/petriDinner/network/transition/Transition.java
+++ src/petriDinner/network/transition/Transition.java
@@ -17,6 +17,10 @@ public class Transition {
         this.placesOut.add(placeOut);
     }
 
+    public void execute() {
+
+    }
+
     public void addPlaceIn(Place placeIn){
         this.placesIn.add(placeIn);
     }
