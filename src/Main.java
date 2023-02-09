ed82e79c101cd4b141afbfc48513d61820d022fb
Andrigo Santos <andrigo.santos@luizalabs.com>
2023-02-09 17:38:26 -0300

    lint

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException, IOException, ParserConfigurationException,SAXException {
+    public static void main(String[] args) throws InterruptedException, IOException, ParserConfigurationException, SAXException {
 
         Examples examples = new Examples();
         Network networkCandyMachine = examples.candyMachine();
@@ -25,27 +25,27 @@ public class Main {
     }
 
     public static String getPathFile(String[] args) {
-        if(args != null && args.length > 0){
+        if (args != null && args.length > 0) {
             return args[0];
         }
 
         String path = "";
         do {
             path = new Scanner(System.in).next();
-        }while (path.length() < 6 || !path.contains(".pnml"));
+        } while (path.length() < 6 || !path.contains(".pnml"));
 
         return path;
     }
 
     public static int getInteractions(String[] args) {
-        if(args != null && args.length > 1){
+        if (args != null && args.length > 1) {
             return Integer.parseInt(args[1]);
         }
 
         int interactions = 0;
         do {
             interactions = new Scanner(System.in).nextInt();
-        }while (interactions > 0);
+        } while (interactions > 0);
 
         return interactions;
     }
